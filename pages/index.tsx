8ee589ff3c03973acc718f550a2d2ff9086f4598
Yuri An <yuriandev@gmail.com>
2022-06-08 12:02:06 +0300

    feat: add Tag component with styles.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Htag, P } from "../components";
+import { Button, Htag, P, Tag } from "../components";
 
 function Home(): JSX.Element {
   return (
@@ -10,6 +10,9 @@ function Home(): JSX.Element {
       <P size="l">Large text size</P>
       <P size="m">Medium text size</P>
       <P size="s">Small text size</P>
+      <Tag size="s">Small size, def color ghost</Tag>
+      <Tag size="m" color="red">Medium size, red</Tag>
+      <Tag color="green">Def size, green</Tag>
 
     </div>
   );
