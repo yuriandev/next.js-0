8454506ef98f783ae7b33b1bdc9c4d1241f4a42d
Yuri An <yuriandev@gmail.com>
2022-06-08 10:42:44 +0300

    feat: add P (paragraph) component with styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { Button, Htag } from "../components";
+import { Button, Htag, P } from "../components";
 
 function Home(): JSX.Element {
   return (
     <div >
       <Htag tag="h1">Header test</Htag>
       <Button appearence="primary" arrow="right">Button</Button>
-      <Button appearence="ghost" arrow="right">Second button</Button>
+      <Button appearence="ghost" arrow="down">Second button</Button>
+      <P size="l">Large text size</P>
+      <P size="m">Medium text size</P>
+      <P size="s">Small text size</P>
+
     </div>
   );
 }
