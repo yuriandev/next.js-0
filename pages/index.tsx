b76286b5e46b8c3b094bdc2ebb12dd97d61b4bec
Yuri An <yuriandev@gmail.com>
2022-06-09 12:54:05 +0300

    feat: add non-editable rating component. TODO: editable rating component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,22 @@
-import React from "react";
-import { Button, Htag, P, Tag } from "../components";
+import React, { useEffect, useState } from "react";
+import { Button, Htag, P, Rating, Tag } from "../components";
 
 function Home(): JSX.Element {
+  const [counter, setCounter] = useState<number>(0);
+
+  useEffect(
+    () => {
+      console.log("Counter is " + counter);
+      return function cleanup() {
+        console.log("Unmount " + counter);
+      };
+    }
+  );
+
   return (
     <div >
-      <Htag tag="h1">Header test</Htag>
-      <Button appearence="primary" arrow="right">Button</Button>
+      <Htag tag="h1">{counter}</Htag>
+      <Button appearence="primary" arrow="right" onClick={() => setCounter(x => x + 1)}>Button</Button>
       <Button appearence="ghost" arrow="down">Second button</Button>
       <P size="l">Large text size</P>
       <P size="m">Medium text size</P>
@@ -13,6 +24,7 @@ function Home(): JSX.Element {
       <Tag size="s">Small size, def color ghost</Tag>
       <Tag size="m" color="red">Medium size, red</Tag>
       <Tag color="green">Def size, green</Tag>
+      <Rating rating={4} />
 
     </div>
   );
