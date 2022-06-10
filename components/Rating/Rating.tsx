f1cb7e1d8973067e9035cb6720bdf4be90ecd4c3
Yuri An <yuriandev@gmail.com>
2022-06-10 20:03:21 +0300

    feat: editable Rating component

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -2,7 +2,8 @@ import { RatingProps } from "./Rating.props";
 import styles from "./Rating.module.css";
 import cn from "classnames";
 import StarIcon from "./star.svg";
-import { useEffect, useState } from "react";
+import { useEffect, useState, KeyboardEvent } from "react";
+import { ReadableStreamDefaultController } from "node:stream/web";
 
 
 export const Rating = ({ isEditable = false, rating, setRating, ...props }: RatingProps): JSX.Element => {
@@ -15,14 +16,45 @@ export const Rating = ({ isEditable = false, rating, setRating, ...props }: Rati
     const constructRating = (currentRating: number) => {
         const updatedArray = ratingArray.map((r: JSX.Element, i: number) => {
             return (
-                < StarIcon
+                <span
                     className={cn(styles.star, {
-                        [styles.filled]: i < currentRating
+                        [styles.filled]: i < currentRating,
+                        [styles.editable]: isEditable
                     })}
-                />
+                    onMouseEnter={() => changeDisplay(i + 1)}
+                    onMouseLeave={() => changeDisplay(rating)}
+                    onClick={() => onClick(i + 1)}
+                >
+                    < StarIcon
+                        tabIndex={isEditable ? 0 : -1}
+                        onKeyDown={(e: KeyboardEvent<SVGElement>) => isEditable && handleSpace(i + 1, e)}
+                    />
+                </span>
+
             );
         });
         setRatingArray(updatedArray);
+    };
+
+    const changeDisplay = (i: number) => {
+        if (!isEditable) {
+            return;
+        }
+        constructRating(i);
+    };
+
+    const onClick = (i: number) => {
+        if (!isEditable || !setRating) {
+            return;
+        }
+        setRating(i);
+    };
+
+    const handleSpace = (i: number, e: KeyboardEvent<SVGElement>) => {
+        if (e.code != "Space" || !setRating) {
+            return;
+        }
+        setRating(i);
     };
 
     return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Button, Htag, P, Rating, Tag } from "../components";
 
 function Home(): JSX.Element {
-  const [counter, setCounter] = useState<number>(0);
-
-  useEffect(
-    () => {
-      console.log("Counter is " + counter);
-      return function cleanup() {
-        console.log("Unmount " + counter);
-      };
-    }
-  );
+  const [rating, setRating] = useState<number>(4);
 
   return (
     <div >
-      <Htag tag="h1">{counter}</Htag>
-      <Button appearence="primary" arrow="right" onClick={() => setCounter(x => x + 1)}>Button</Button>
+      <Htag tag="h1">Hello</Htag>
+      <Button appearence="primary" arrow="right">Button</Button>
       <Button appearence="ghost" arrow="down">Second button</Button>
       <P size="l">Large text size</P>
       <P size="m">Medium text size</P>
@@ -24,7 +15,7 @@ function Home(): JSX.Element {
       <Tag size="s">Small size, def color ghost</Tag>
       <Tag size="m" color="red">Medium size, red</Tag>
       <Tag color="green">Def size, green</Tag>
-      <Rating rating={4} />
+      <Rating rating={rating} isEditable setRating={setRating} />
 
     </div>
   );
