50233d3a47b86692664faadddb3bf6735dc2f7af
Yuri An <yuriandev@gmail.com>
2022-06-13 12:15:48 +0300

    feat: add base layouts

---
+++ layout/Layout.tsx
@@ -0,0 +1,21 @@
+import { Footer } from "./Footer/Footer";
+import { Header } from "./Header/Header";
+import { LayoutProps } from "./Layout.props";
+import { Sidebar } from "./Sidebar/Sidebar";
+import React from "react";
+
+
+
+export const Layout = ({ children }: LayoutProps): JSX.Element => {
+    return (
+        <>
+            <Header />
+            <div>
+                <Sidebar />
+                <div>{children}</div>
+            </div>
+            <Footer />
+
+        </>
+    );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { Button, Htag, P, Rating, Tag } from "../components";
+import { Layout } from "../layout/Layout";
 
 function Home(): JSX.Element {
   const [rating, setRating] = useState<number>(4);
 
   return (
-    <div >
+    <Layout >
       <Htag tag="h1">Hello</Htag>
       <Button appearence="primary" arrow="right">Button</Button>
       <Button appearence="ghost" arrow="down">Second button</Button>
@@ -17,7 +18,7 @@ function Home(): JSX.Element {
       <Tag color="green">Def size, green</Tag>
       <Rating rating={rating} isEditable setRating={setRating} />
 
-    </div>
+    </Layout>
   );
 }
 
