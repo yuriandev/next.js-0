4eaaf5d0c664f1ad48536065ce5349d79fd4e45a
Yuri An <yuriandev@gmail.com>
2022-07-24 11:27:40 +0300

    feat: add Link component

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -8,6 +8,7 @@ import ServicesIcon from "./icons/services.svg";
 import BooksIcon from "./icons/books.svg";
 import ProductsIcon from "./icons/products.svg";
 import { TopLevelCategory } from "../../interfaces/page.interface";
+import Link from "next/link";
 
 
 const firstLevelMenu: FirstLevelMenuItem[] = [
@@ -48,14 +49,18 @@ export const Menu = (): JSX.Element => {
             <>
                 {firstLevelMenu.map(m => (
                     <div key={m.route}>
-                        <a href={`/${m.route}`}>
-                            <div className={cn(styles.firstLevel, {
-                                [styles.firstLevelActive]: m.id == firstCategory
-                            })}>
-                                {m.icon}
-                                <span>{m.name}</span>
-                            </div>
-                        </a>
+                        <Link href={`/${m.route}`}>
+                            <a>
+                                <div className={cn(styles.firstLevel, {
+                                    [styles.firstLevelActive]: m.id == firstCategory
+                                })}>
+                                    {m.icon}
+                                    <span>{m.name}</span>
+                                </div>
+                            </a>
+
+                        </Link>
+
                         {m.id == firstCategory && buildSecondLevel(m)}
                     </div>
                 ))}
@@ -84,9 +89,12 @@ export const Menu = (): JSX.Element => {
     const buildThirdLevel = (pages: PageItem[], route: string) => {
         return (
             pages.map(p => (
-                <a href={`/${route}/${p.alias}`} className={cn(styles.thirdLevel, {
-                    [styles.thirdLevelActive]: false
-                })}>{p.category}</a>
+                <Link href={`/${route}/${p.alias}`}>
+                    <a className={cn(styles.thirdLevel, {
+                        [styles.thirdLevelActive]: false
+                    })}>{p.category}
+                    </a>
+                </Link>
             ))
         );
     };
